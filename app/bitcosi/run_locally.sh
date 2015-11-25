94d7b89d0888003bdaffa6c0cc49fe55b36b1a3a
Linus Gasser <linus.gasser@epfl.ch>
2015-11-25 05:52:10 +0100

    added new argument for run-level

--- app/bitcosi/run_locally.sh
+++ app/bitcosi/run_locally.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+DBG=${4:-1}
 KEY_DIR=local_keys
 KEYS=$KEY_DIR/key
 HOSTLIST=$KEY_DIR/hostlist
 NUMBER=${1:-2}
+COMMAND=bitcosi
 rm -f $HOSTLIST
 
 rm -rf $KEY_DIR
@@ -11,13 +13,13 @@ mkdir $KEY_DIR
 
 for a in $( seq 1 $NUMBER ); do
   PORT=$(( 2000 + $a * 10 ))
-  ./bitcosi keygen localhost:$PORT -key $KEYS$a
+  ./$COMMAND keygen localhost:$PORT -key $KEYS$a
 done
 cat $KEYS*.pub >> $HOSTLIST
 
-./bitcosi build $HOSTLIST
+./$COMMAND build $HOSTLIST
 
 for a in $( seq 2 $NUMBER ); do
-  ./bitcosi run -key $KEYS$a &
+  ./$COMMAND -debug $DBG run -key $KEYS$a &
 done
-./bitcosi -debug 3 run -key ${KEYS}1
+./$COMMAND -debug $DBG run -key ${KEYS}1
